72b4a128f766abdd73b147730a3ff2706ff38e0c
john <john@spinningplates.org>
2020-07-12 18:13:44 -0700

    09-13 complete

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,31 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(num):
+    if num % 2 == 0:
+        print(True)
+    else:
+        print(False)
+
 
 # Read a number from the keyboard
+
+
 num = input("Enter a number: ")
 num = int(num)
+
+is_even(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+
+def even_or_odd(num):
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
+
+
+even_or_odd(num)
